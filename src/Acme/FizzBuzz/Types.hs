5bde6437e9d0ead297962ddc3be8c799f38c7ee0
cheater <cheater00@gmail.com>
2017-07-10 03:40:16 +0200

    FizzBuzz type related function

--- src/Acme/FizzBuzz/Types.hs
+++ src/Acme/FizzBuzz/Types.hs
@@ -1,10 +1,22 @@
 module Acme.FizzBuzz.Types where
 
-data ComplexNumber = DivBy3 | DivBy5 | DivBy15  | Prime    | OtherNum Int
+-- |The 'NumberClass' type tells us what sort of number we are dealing with
+-- for the purposes of 'FizzBuzz'.
+data NumberClass = DivBy3 | DivBy5 | DivBy15  | Prime    | OtherNum Int
   deriving (Eq, Show)
 
+-- |The 'FizzBuzz' type tells us if we Fizz or Buzz.
 data FizzBuzz      = Buzz   | Fizz   | FizzBuzz | BuzzFizz | OtherFB  Int
   deriving Eq
+
+ncToFb :: NumberClass -> FizzBuzz
+ncToFb x
+  | x == DivBy3  = Buzz
+  | x == DivBy5  = Fizz
+  | x == DivBy15 = FizzBuzz
+  | x == Prime   = BuzzFizz
+
+ncToFb (OtherNum n) = OtherFB n
 
 instance Show FizzBuzz where
   show Buzz      = "Buzz"
