860c2429ef0369b0851c6c082268143548c22647
cheater <cheater00@gmail.com>
2017-07-10 06:39:53 +0200

    FizzBuzz related functions.

--- src/Acme/FizzBuzz.hs
+++ src/Acme/FizzBuzz.hs
@@ -1,2 +1,10 @@
 module Acme.FizzBuzz where
 
+import Math.Fibonacci (fib_fast)
+import Math.FizzBuzz (classifyNumber)
+import Acme.FizzBuzz.Types
+
+-- |'fibFb' takes an index into the Fibonacci numbers, and returns the correct
+-- FizzBuzz classification for it.
+fibFb :: Int -> FizzBuzz
+fibFb = ncToFb . classifyNumber . fib_fast
---
+++ src/Math/FizzBuzz.hs
@@ -0,0 +1,30 @@
+{-|
+Module      : Math.FizzBuzz
+Description : Math functions for FizzBuzz
+License     : MIT
+Maintainer  : cheater00@gmail.com
+
+This module contains math functions for the FizzBuzz algorithm that don't
+really belong in any other module.
+-}
+module Math.FizzBuzz where
+
+import Acme.FizzBuzz.Types
+import Math.NumberTheory.Primes.Testing (isPrime)
+
+dividesBy3 :: Integer -> Bool
+dividesBy3 x = (x `mod` 3) == 0
+
+dividesBy5 :: Integer -> Bool
+dividesBy5 x = (x `mod` 5) == 0
+
+classifyNumber x
+  |      divBy3  && (not divBy5) = DivBy3
+  | (not divBy3) &&      divBy5  = DivBy5
+  |      divBy3  &&      divBy5  = DivBy15
+  | prime                        = Prime
+  | otherwise                    = OtherNum x
+  where
+    divBy3 = dividesBy3 x
+    divBy5 = dividesBy5 x
+    prime = isPrime x
